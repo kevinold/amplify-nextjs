c86590de7fe2f5704953769ab3e7a4161e3b30e2
Kevin Old <kevin@kevinold.com>
2022-11-09 14:25:22 -0600

    configure amplify in _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,10 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@aws-amplify/ui-react/styles.css";
+import { Amplify } from 'aws-amplify';
+import type { AppProps } from 'next/app';
+import awsExports from "../src/aws-exports";
+import '../styles/globals.css';
+
+Amplify.configure({ ...awsExports, ssr: true });
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
